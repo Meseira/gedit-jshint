694df73713ad084386a1988f5bb1389a7ab9dddd
Xavier Gendre <gendre.reivax@gmail.com>
2016-01-10 17:14:51 +0100

    Add a menu item and an accelerator for a test action

--- jshint/appactivatable.py
+++ jshint/appactivatable.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import GObject, Gedit
+from gi.repository import GLib, GObject, Gedit, Gio
 
 class AppActivatable(GObject.Object, Gedit.AppActivatable):
     __gtype_name__ = "JSHintAppActivatable"
@@ -24,9 +24,21 @@ class AppActivatable(GObject.Object, Gedit.AppActivatable):
 
     def __init__(self):
         GObject.Object.__init__(self)
+        self._menu_ext = None
 
     def do_activate(self):
-        pass
+        action = Gio.SimpleAction(name="check-with-jshint")
+        action.connect("activate",
+                lambda action, data: print("Test JSHint Plugin"))
+        self.app.add_action(action)
+
+        self.app.set_accels_for_action("app.check-with-jshint", ["<Ctrl>J"])
+
+        self._menu_ext = self.extend_menu("tools-section")
+        item = Gio.MenuItem.new("Check with JSHint", "app.check-with-jshint")
+        self._menu_ext.append_menu_item(item)
 
     def do_deactivate(self):
-        pass
+        self._menu_ext = None
+        self.app.set_accels_for_action("app.check-with-jshint", [])
+        self.app.remove_action("check-with-jshint")
